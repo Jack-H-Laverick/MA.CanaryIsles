b25f42cccd6ff9772519b201ed9a64e10224f41f
Jack H Laverick from Strath <jacklaverick@ymail.com>
2022-05-06 11:05:00 +0100

    feat: Add overhang module to model implementation.

---
+++ R scripts/bathymetry/bathymetry.03 DEFINE OVERHANG.R
@@ -0,0 +1,65 @@
+
+# Create an object defining the geographic extent of the model domain
+
+#### Set up ####
+
+rm(list=ls())                                                   
+
+Packages <- c("tidyverse", "sf", "stars", "rnaturalearth", "raster")                  # List handy packages
+lapply(Packages, library, character.only = TRUE)                            # Load packages
+
+source("./R scripts/@_Region file.R")                                       # Define project region 
+
+world <- ne_countries(scale = "medium", returnclass = "sf") %>%             # Get a world map
+  st_transform(crs = crs)                                                   # Assign polar projection
+
+EEZ <- read_sf("./Data/eez")
+
+GEBCO <- read_stars("../Shared data/GEBCO_2020.nc")
+st_crs(GEBCO) <- st_crs(EEZ)
+GFW <- raster("../Shared data/distance-from-shore.tif")
+
+crop <- as(extent(-22, -9, 24, 30), "SpatialPolygons")
+crs(crop) <- crs(GEBCO)
+
+GFW <- crop(GFW, crop)
+
+mask <- readRDS("./Objects/Domains.rds") %>%  filter(Shore == "Offshore")
+
+#### Polygons based on depth ####
+
+Depths <- GEBCO[EEZ] %>% 
+  st_as_stars()
+
+Depths[[1]][Depths[[1]] > units::set_units(0, "m") | Depths[[1]] < units::set_units(-600, "m")] <- NA
+
+Depths[[1]][is.finite(Depths[[1]])] <- units::set_units(-600, "m")
+
+Bottom <- st_as_stars(Depths) %>%
+  st_as_sf(merge = TRUE) %>%
+  st_make_valid() %>%
+  group_by(GEBCO_2020.nc) %>%
+  summarise(Depth = abs(mean(GEBCO_2020.nc))) %>%
+  st_make_valid()
+
+ggplot(Bottom) +
+  geom_sf(aes(fill = Depth), alpha = 0.2) +
+  theme_minimal()
+
+#### Cut to domain ####
+
+clipped <- st_difference(mask, st_transform(Bottom, crs = st_crs(mask)))
+
+ggplot(clipped) +
+  geom_sf(aes(fill = Depth), alpha = 0.5)
+
+#### Format to domains object ####
+
+overhang <- transmute(clipped, 
+                      Shore = "Offshore",
+                      area = as.numeric(st_area(clipped)),
+                      Elevation = exactextractr::exact_extract(raster("../Shared data/GEBCO_2020.nc"), clipped, "mean")) %>% 
+  st_transform(crs = crs)
+
+saveRDS(overhang, "./Objects/Overhang.rds")
+
